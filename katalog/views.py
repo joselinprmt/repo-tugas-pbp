014fe50951ce2b31c59c4b6aaa25f88f5f38728f
joselinprmt <joselin.permata@ui.ac.id>
2022-09-14 01:07:29 +0700

    Mengedit file katalog.html, urls, views

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Joselin Permata Aprillia',
+        'npm': '2106705410'
+    }
+    return render(request, "katalog.html", context)
